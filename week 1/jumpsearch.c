de81e176dcbd0f7c1c4b0fb15f2cd7b14bc8b9a0
MANAS JOSHI <158481955+manasjoshi275@users.noreply.github.com>
2025-02-15 20:45:22 +0530

    Update jumpsearch.c

--- week 1/jumpsearch.c
+++ week 1/jumpsearch.c
@@ -50,3 +50,63 @@ int main() {
 
     return 0;
 }
+/*
+#include <stdio.h>
+#include <math.h>
+
+int jumpsearch(int arr[], int n, int key) {
+    int start = 0;
+    int jump = sqrt(n);
+    int comparisons = 0;
+    while (arr[start] < key && start < n) {
+        comparisons++; 
+        int prev = start;
+        start = jump;
+        jump += sqrt(n);
+        if (start >= n) {
+            start = prev;
+            break;
+        }
+    }
+    while (start < n) {
+        comparisons++;
+        if (arr[start] == key) {
+            return comparisons;
+        }
+        if (arr[start] > key) {
+            break;
+        }
+        start++;
+    }
+    
+    return comparisons;
+}
+
+int main() {
+    int test;
+    printf("Enter the number of test cases!\n");
+    scanf("%d", &test);
+    
+    while (test--) {
+        int n;
+        printf("Enter the array size!\n");
+        scanf("%d", &n);
+        
+        int arr[n];
+        printf("Enter the array elements in sorted order!\n");
+        
+        int i = 0;
+        while (i < n) {
+            scanf("%d", &arr[i]);
+            i++;
+        }
+        
+        int key;
+        printf("Enter the key to be searched in array!\n");
+        scanf("%d", &key);
+        
+        int comparisons = jumpsearch(arr, n, key);
+        printf("Number of comparisons required to search key: %d\n", comparisons);
+    }
+    return 0;
+}*/
